2e2787e0ae37715f7afed7791013d5191f98b460
seamusk84 <seamusk84@gmail.com>
2025-09-04 16:38:04 +0100

    Update debug-env.ts

--- pages/api/debug-env.ts
+++ pages/api/debug-env.ts
@@ -2,17 +2,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  const out = {
+  res.status(200).json({
     ok: true,
     supabaseUrlPresent: !!process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseKeyPresent: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    supabaseAnonPresent: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     serviceKeyPresent: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
     moderatorTokenPresent: !!process.env.MODERATOR_TOKEN,
-    captchaKeysPresent:
-      !!process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY &&
-      !!process.env.HCAPTCHA_SECRET,
+    captchaConfigured:
+      !!process.env.HCAPTCHA_SECRET && !!process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY,
     rateLimitSecretPresent: !!process.env.RATE_LIMIT_SECRET,
-    rateLimitPerHour: process.env.RATE_LIMIT_PER_HOUR ?? "(unset)",
-  };
-  res.status(200).json(out);
+    rateLimitPerHour: process.env.RATE_LIMIT_PER_HOUR || "(unset)",
+  });
 }
