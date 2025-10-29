d91fc6d732b573a0f84dbeeddf19470cef91fdfe
seamusk84 <seamusk84@gmail.com>
2025-10-29 09:26:03 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,6 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
-import Head from "next/head";
-
-// ⬇️ change these two lines:
 import "../styles/globals.css";
-import Header from "../components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>StreetSage</title>
-      </Head>
-      <Header />
-      <main className="container">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
